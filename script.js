473d8b49eb763c0d0da45ea93dfb9b5bd1251187
Humberto <humbertolima108@gmail.com>
2023-12-02 18:34:25 -0300

    fix logic bug ang clean code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ readline.question(`Digite um número: `, (num) => {
   const array = [];
   const newNumber = Number(num);
 
-  if (Number.isInteger(newNumber && newNumber >= 0)) {
+  if (Number.isInteger(newNumber) && newNumber >= 0) {
     for (let i = num - 1; i > 0; i--) {
       if (i % 3 === 0 || i % 5 === 0) {
         array.push(i);
@@ -17,7 +17,7 @@ readline.question(`Digite um número: `, (num) => {
     console.log(`Esses são os números aptos: ${array}`);
     console.log(`Essa é a soma deles:${total}`);
   } else {
-    console.log(`digite um numero inteiro positivo`);
+    console.log(`Digite um numero inteiro positivo`);
   }
 
   readline.close();
